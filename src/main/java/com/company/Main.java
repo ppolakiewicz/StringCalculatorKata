9ff01f089cd66aee04a80ac579dd8decff90e992
Paweł Polakiewicz <pp.polakiewicz@gmail.com>
2020-11-25 21:22:39 +0100

    Fifth point complete

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,31 +1,36 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
+
+    public static final String NEGATIVES_MESSAGE = "negatives not allowed: ";
 
     private static final String DELIMITER_MARKER = "//";
     private static final int MAX_ITEM_VALUE = 1000;
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
     }
 
-    public static int add(String numbers){
-        if(numbers.isEmpty()) return 0;
-        if(numbers.endsWith("\n")) throw new IllegalArgumentException();
+    public static int add(String numbers) {
+        if (numbers.isEmpty()) return 0;
+        if (numbers.endsWith("\n")) throw new IllegalArgumentException();
 
         DelimiterDTO delimiterDTO;
-
-        if(numbers.startsWith(DELIMITER_MARKER))
+        if (numbers.startsWith(DELIMITER_MARKER))
             delimiterDTO = getDelimiter(numbers);
         else
             delimiterDTO = new DelimiterDTO(",", numbers);
 
+        validateNegatives(delimiterDTO);
         return add(delimiterDTO);
     }
 
-    public static DelimiterDTO getDelimiter(String input){
+    public static DelimiterDTO getDelimiter(String input) {
         String[] splitResult = input.split("\n", 2);
 
         return new DelimiterDTO(
@@ -34,12 +39,30 @@ public class Main {
         );
     }
 
-    private static int add(DelimiterDTO delimiterDTO){
-        return Arrays.stream(delimiterDTO.getInput().split(delimiterDTO.getDelimiter()))
-                .map(String::trim)
-                .mapToInt(Integer::parseInt)
+    private static int add(DelimiterDTO delimiterDTO) {
+        return getIntStream(delimiterDTO)
                 .filter(i -> i < MAX_ITEM_VALUE)
                 .sum();
+    }
+
+    private static String getNegativeNumbers(DelimiterDTO delimiterDTO) {
+        return getIntStream(delimiterDTO)
+                .filter(i -> i < 0)
+                .mapToObj(Objects::toString)
+                .collect(Collectors.joining(", "));
+    }
+
+    private static IntStream getIntStream(DelimiterDTO delimiterDTO) {
+        return Arrays.stream(delimiterDTO.getInput().split(delimiterDTO.getDelimiter()))
+                .map(String::trim)
+                .mapToInt(Integer::parseInt);
+    }
+
+    private static void validateNegatives(DelimiterDTO delimiterDTO){
+        String negativeNumbers = getNegativeNumbers(delimiterDTO);
+
+        if(negativeNumbers.length() > 0)
+            throw new IllegalArgumentException(NEGATIVES_MESSAGE + negativeNumbers);
     }
 
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
+import static com.company.Main.NEGATIVES_MESSAGE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -24,6 +25,19 @@ public class MainTest {
 
         //then
         assertEquals(exception.getMessage(), new IllegalArgumentException().getMessage());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideStringsWithNegatives")
+    public void stringWithNegativeNumbersThrowException(String input, String errorMessage){
+
+        //when
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class, () -> Main.add(input)
+        );
+
+        //then
+        assertEquals(exception.getMessage(), errorMessage);
     }
 
     @ParameterizedTest
@@ -50,6 +64,18 @@ public class MainTest {
                 Arguments.of("//}\n1}2}2000", 3),
                 Arguments.of("1,2,3,1001", 6),
                 Arguments.of("//@\n1@2001", 1)
+        );
+    }
+
+    private static Stream<Arguments> provideStringsWithNegatives() {
+        return Stream.of(
+                Arguments.of("-1", NEGATIVES_MESSAGE + "-1"),
+                Arguments.of("-1,-2", NEGATIVES_MESSAGE + "-1, -2"),
+                Arguments.of("1,2,-3\n-4", NEGATIVES_MESSAGE + "-3, -4"),
+                Arguments.of("1\n-2\n3\n-4", NEGATIVES_MESSAGE + "-2, -4"),
+                Arguments.of("//;\n -1;2;3\n4", NEGATIVES_MESSAGE + "-1"),
+                Arguments.of("//}\n1}2}-2000", NEGATIVES_MESSAGE + "-2000"),
+                Arguments.of("1,2,-3,-1001", NEGATIVES_MESSAGE + "-3, -1001")
         );
     }
 }
